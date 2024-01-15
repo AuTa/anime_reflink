a689a3fee398aaeac547d86399d27374ea605df2
AuTa <auta520@live.com>
2024-01-15 14:08:02 +0800

    :bug: fix: 用 PartialEq derive 替代 eq 实现
    如果在实现 PartialEq 时进行比较，会死循环。

--- src/source_anime_map.rs
+++ src/source_anime_map.rs
@@ -1,30 +1,13 @@
 use serde::{Deserialize, Serialize};
 
 // 文件类型.
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[repr(u8)]
 pub enum FileType {
     File,                         // 普通文件.
     Dir,                          // 文件夹.
     Other,                        // ".parts" 文件.
     Nesting(Vec<SourceAnimeMap>), // 文件夹里还是文件夹.
-}
-
-// 实现 PartialEq trait.
-impl PartialEq for FileType {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            // 嵌套类型先比较长度，再比较内容.
-            (FileType::Nesting(x), FileType::Nesting(y)) => {
-                if x.len() != y.len() {
-                    false
-                } else {
-                    x.iter().eq(y.iter()) // 迭代器比较.
-                }
-            }
-            // 其它类型直接返回比较结果
-            (x, y) => x == y,
-        }
-    }
 }
 
 // 文件夹映射.
